bcdcd82b1c0404bbbd66c6400773b11c26a122df
lat9 <lat9@vinosdefrutastropicales.com>
2024-09-30 15:32:06 -0400

    Refactoring/reformatting tpl_footer.php
    Similar changes as the base zc210 PR.

--- includes/templates/bootstrap/common/tpl_footer.php
+++ includes/templates/bootstrap/common/tpl_footer.php
@@ -17,19 +17,18 @@
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: DrByte 2020 Dec 29 Modified in v1.5.8-alpha $
  */
-require(DIR_WS_MODULES . zen_get_module_directory('footer.php'));
-?>
+require DIR_WS_MODULES . zen_get_module_directory('footer.php');
 
-<?php
 if (!isset($flag_disable_footer) || !$flag_disable_footer) {
 ?>
-
 <div id="footerWrapper">
 
 <!--bof-navigation display -->
-<?php if (EZPAGES_STATUS_FOOTER == '1' or (EZPAGES_STATUS_FOOTER == '2' && zen_is_whitelisted_admin_ip())) { ?>
-<?php require($template->get_template_dir('tpl_ezpages_bar_footer.php',DIR_WS_TEMPLATE, $current_page_base,'templates'). '/tpl_ezpages_bar_footer.php'); ?>
-<?php } ?>
+<?php
+    if (EZPAGES_STATUS_FOOTER === '1' || (EZPAGES_STATUS_FOOTER === '2' && zen_is_whitelisted_admin_ip())) {
+        require $template->get_template_dir('tpl_ezpages_bar_footer.php', DIR_WS_TEMPLATE, $current_page_base, 'templates') . '/tpl_ezpages_bar_footer.php';
+    }
+?>
 <!--eof-navigation display -->
 <?php
     // -----
@@ -39,47 +38,45 @@ if (!isset($flag_disable_footer) || !$flag_disable_footer) {
 ?>
 <!--bof-ip address display -->
 <?php
-if (SHOW_FOOTER_IP == '1') {
+    if (SHOW_FOOTER_IP === '1') {
 ?>
-<div id="siteinfoIP" class="text-center"><?php echo TEXT_YOUR_IP_ADDRESS . '  ' . $_SERVER['REMOTE_ADDR']; ?></div>
+    <div id="siteinfoIP" class="text-center"><?= TEXT_YOUR_IP_ADDRESS . '  ' . $_SERVER['REMOTE_ADDR'] ?></div>
 <?php
-}
+    }
 ?>
 <!--eof-ip address display -->
 
 <!--bof-banner #5 display -->
 <?php
-  if (SHOW_BANNERS_GROUP_SET5 != '' && $banner = zen_banner_exists('dynamic', SHOW_BANNERS_GROUP_SET5)) {
-    if ($banner->RecordCount() > 0) {
-$find_banners = zen_build_banners_group(SHOW_BANNERS_GROUP_SET5);
-$banner_group = 5;
+    if (SHOW_BANNERS_GROUP_SET5 != '' && $banner = zen_banner_exists('dynamic', SHOW_BANNERS_GROUP_SET5)) {
+        if (!$banner->EOF) {
+            $find_banners = zen_build_banners_group(SHOW_BANNERS_GROUP_SET5);
+            $banner_group = 5;
 ?>
-
-<div class="zca-banner bannerFive rounded">
+    <div class="zca-banner bannerFive rounded">
 <?php 
-if (ZCA_ACTIVATE_BANNER_FIVE_CAROUSEL == 'true') {
-require($template->get_template_dir('tpl_zca_banner_carousel.php',DIR_WS_TEMPLATE, $current_page_base,'common'). '/tpl_zca_banner_carousel.php'); 
-} else {
-echo zen_display_banner('static', $banner);
-}
+            if (ZCA_ACTIVATE_BANNER_FIVE_CAROUSEL === 'true') {
+                require $template->get_template_dir('tpl_zca_banner_carousel.php', DIR_WS_TEMPLATE, $current_page_base, 'common') . '/tpl_zca_banner_carousel.php'; 
+            } else {
+                echo zen_display_banner('static', $banner);
+            }
 ?>
-</div>
+    </div>
 <?php
+        }
     }
-  }
 ?>
 <!--eof-banner #5 display -->
 
 <!--bof- site copyright display -->
-<div id="siteinfoLegal" class="legalCopyright text-center"><?php echo FOOTER_TEXT_BODY; ?></div>
+    <div id="siteinfoLegal" class="legalCopyright text-center"><?= FOOTER_TEXT_BODY ?></div>
 <!--eof- site copyright display -->
-
-
 </div>
 <?php
 } // flag_disable_footer
-?>
 
-<?php if (false || (isset($showValidatorLink) && $showValidatorLink == true)) { ?>
-<a href="https://validator.w3.org/check?uri=<?php echo urlencode('http' . ($request_type == 'SSL' ? 's' : '') . '://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] . (strstr($_SERVER['REQUEST_URI'], '?') ? '&' : '?') . zen_session_name() . '=' . zen_session_id()); ?>" rel="noopener" target="_blank">VALIDATOR</a>
-<?php } ?>
+if (false || !empty($showValidatorLink)) {
+?>
+<a href="https://validator.w3.org/check?uri=<?= urlencode('http' . ($request_type == 'SSL' ? 's' : '') . '://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] . (strstr($_SERVER['REQUEST_URI'], '?') ? '&' : '?') . zen_session_name() . '=' . zen_session_id()) ?>" rel="noopener" target="_blank">VALIDATOR</a>
+<?php
+}
